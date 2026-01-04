629c9232b7e996688c2ba6b23b2e141a4ee5720d
mekdelawit <mekdimeki8@gmail.com>
2026-01-04 12:21:20 +0300

    Add files via upload

---
+++ src/Class1.java
@@ -0,0 +1,17 @@
+package class;
+
+
+/**
+ * @author kiot
+ * @version 1.0
+ */
+public class Class1 {
+
+	public Class1(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+}//end Class1
---
+++ src/customer.java
@@ -0,0 +1,29 @@
+package class;
+
+
+/**
+ * @author kiot
+ * @version 1.0
+
+ */
+public class customer extends user {
+
+	public booking m_booking;
+
+	public customer(){
+
+	}
+
+	public void finalize() throws Throwable {
+		super.finalize();
+	}
+	/**
+	 * 
+	 * @param carId
+	 * @param startDate
+	 * @param endDate
+	 */
+	public bookRental(int carId, date startDate, date endDate){
+
+	}
+}//end customer
