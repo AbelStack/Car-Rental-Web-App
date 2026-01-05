0998a9ee679e2638e58e881d95f3e6273d3bb1c6
biniyam-rgb <binigebrie853@gmail.com>
2026-01-04 23:22:58 -0800

    Add files via upload

---
+++ Docs/date.java
@@ -0,0 +1,17 @@
+package class;
+
+
+/**
+ * @author kiot
+ * @version 1.0
+ */
+public class date {
+
+	public date(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+}//end date
---
+++ Docs/driver.java
@@ -0,0 +1,27 @@
+package class;
+
+
+/**
+ * @author kiot
+ * @version 1.0
+ */
+public class driver extends user {
+
+	public string licenseNO.;
+
+	public driver(){
+
+	}
+
+	public void finalize() throws Throwable {
+		super.finalize();
+	}
+	/**
+	 * 
+	 * @param bookingId
+	 * @param status
+	 */
+	public void updateBookingStatus(int bookingId, string status){
+
+	}
+}//end driver
